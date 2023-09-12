8f1b404c4058da300c02c044ef3c56b76e43a0df
Autophile-Dev <waleed.zaheer55@gmail.com>
2023-09-12 14:59:49 +0500

    Pic check

--- routes/users.js
+++ routes/users.js
@@ -111,44 +111,47 @@ const upload = multer({ storage });
 
 router.post('/create-user', upload.single('userImage'), async (req, res) => {
     try {
-        const { firstName, lastName, email, phoneNum, password, address, city, dob } = req.body;
-
-        // Check if user exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Email already exists' });
-        }
-
-        // Remember to hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Upload the image to Cloudinary
-        const imageUploadResult = await cloudinary.uploader.upload(req.file.buffer.toString('base64'), {
-            folder: 'user-images', // Optional: specify a folder in your Cloudinary account
-            allowed_formats: ['jpg', 'png', 'jpeg'],
-        });
-
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            phoneNum,
-            password: hashedPassword,
-            userImage: imageUploadResult.secure_url,
-            address,
-            city,
-            dob,
-            createdAt: new Date(),
-        });
-
-        await newUser.save();
-        return res.status(200).json({ cmpUser: newUser });
+      const { firstName, lastName, email, phoneNum, password, address, city, dob } = req.body;
+  
+      // Check if user exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'Email already exists' });
+      }
+  
+      // Remember to hash the password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      // Upload the image to Cloudinary
+      const imageUploadResult = await cloudinary.uploader.upload(req.file.buffer.toString('base64'), {
+        folder: 'user-images', // Optional: specify a folder in your Cloudinary account
+        allowed_formats: ['jpg', 'png', 'jpeg'],
+      });
+  
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        phoneNum,
+        password: hashedPassword,
+        userImage: imageUploadResult.secure_url,
+        address,
+        city,
+        dob,
+        createdAt: new Date(),
+      });
+  
+      await newUser.save();
+      
+      // Send a simplified response with just user information
+      return res.status(200).json({ message: 'User created successfully', cmpUser: { firstName, lastName, email, phoneNum, address, city, dob } });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+  });
+  
 
 router.post('/user-login', async (req, res) => {
     try {
